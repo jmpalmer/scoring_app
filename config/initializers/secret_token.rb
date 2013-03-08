9f1b45793466a55b681b697ed3cd4b64ec9148e8
Jon Palmer <jmpalme4@gmail.com>
2013-03-07 22:24:04 -0500

    blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoringApp::Application.config.secret_token = '2f72489c9697cff79867b79f54eccbcd9eaa783a486eff581d9e29f3130f57a514f2d87bf9ba62716b26506d905dcb33abf4102d0c25ef6458739c52e90d8cb4'
